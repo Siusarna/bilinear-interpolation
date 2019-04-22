f576452c26ca66f09a5f1f98b039814bfc6a0898
Siusarna <charodeyrap@gmail.com>
2019-04-22 19:44:58 +0300

    clear code

--- Project1/Project1/image.h
+++ Project1/Project1/image.h
@@ -42,40 +42,44 @@ class image {
 	BMPHEAD info; //Temporary
 	int bytesPerLine;
 	PIXELDATA **arr;
+	int padding;
 
 public:
 	image(string path) {
 		this->path = path;
-		ifstream fin(path, ios::binary);
-		fin.read((char*)&info, sizeof(info));
-		cout << info.depth << endl;
 
-		PIXELDATA rgb_l;
+		PIXELDATA rgb_l; //empty pixel
+
 		//int row = (width * 3 + 3) & (~3);
-		FILE * f1, *f2;
-
+		FILE * f1;
 		f1 = fopen("t2_24.bmp", "rb");
-		f2 = fopen("result.bmp", "wb");
 
-		size_t padding = 0;//смещение байт
+		fread(&this->info, sizeof(info), 1, f1);
+
+		size_t padding = 0;//зміщення байт
 		if ((this->info.width * 3) % 4)
 		{
 			padding = 4 - (this->info.width * 3) % 4;
 		};
+		this->padding = padding;
 		this->arr = new PIXELDATA*[this->info.depth];
 		for(int i=0;i<this->info.depth;i++){
 			arr[i] = new PIXELDATA[this->info.width];
-			/*for (int j = 0; j < width * 3; j += 3) {
-				temp = pixel[j];
-				pixel[j] = pixel[j + 2];
-				pixel[j + 2] = temp;
-			}*/
 		}
 		for (int i = 0; i < this->info.depth; i++) {
 			fread(arr[i], sizeof(PIXELDATA), this->info.width, f1);
 			if (padding != 0) fread(&rgb_l, padding, 1, f1);
 		}
-		fin.close();
+		
+	}
+	image(image& first, double coefficient) //TODO: image incease algorithm (use this constructor, Siusarna)
+	{
+		FILE *f2;
+		f2 = fopen("result.bmp", "wr");
+
+		PIXELDATA rgb_l;
+
+		fwrite(&this->info, sizeof(info), 1, f2);
 		for (int i = 0; i < this->info.depth; i++)
 		{
 			fwrite(this->arr[i], sizeof(arr), this->info.width, f2);
@@ -85,14 +89,4 @@ public:
 			}
 		}
 	}
-	/*image(image& first, double coefficient) //TODO: image incease algorithm (use this constructor, Siusarna)
-	{
-		ofstream out;
-		out.open("result.bmp", ios::binary);
-		out.write((char*)&first.info, sizeof(first.info));
-		for (int i = 0; i < arr.size(); i++) {
-			out.write((char*)&first.arr, sizeof(first.arr));
-		}
-		out.close();
-	}*/
 };
