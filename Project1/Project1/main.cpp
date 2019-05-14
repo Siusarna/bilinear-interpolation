d7b6292bae26442f09d6b2b54594635dc53a7fce
Artem Supryhan <suprigan.artem1@gmail.com>
2019-05-15 01:19:16 +0300

    gg wp

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
 	imageSaver saver;
 	image first = reader.read(pathRead);
 	image second = resizer.resize(first, coefficient);
+	cout << "Enlarging image "<< coefficient << " times... Done."<<endl;
+	cout << "Written result to " << pathSave <<endl;
 	saver.save(second, pathSave);
 	system("pause");
 	return 0;
